cfba4a5d8163f9a3cb7bfe7856c7be2fc7615ab5
Gabriela Camargo <101142324+GabiCmg@users.noreply.github.com>
2022-09-21 22:28:56 -0300

    Delete manipulacao_arquivos_write.py

--- Python/manipulacao_arquivos_write.py
+++
@@ -1,6 +0,0 @@
-with open('Aplicacoes/arquivos/dom_casmurro_cap_1.txt','w', encoding='utf-8') as arquivo:
-    arquivo.write('Essa é uma linha que eu escrevi usando Python\n')
-    arquivo.write('Essa é a segunda linha que eu escrevi usando Python\n')
-
-with open('Aplicacoes/arquivos/dom_casmurro_cap_1.txt','r', encoding='utf-8') as arquivo:
-    print(arquivo.read(), end='')
